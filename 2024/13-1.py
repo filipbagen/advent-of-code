e46521788aad7e1775e2da5bff61ba844b2654e5
Filip Malm-Bägén <Filip.bagen@gmail.com>
2025-01-03 10:56:54 -0800

    Day 13

---
+++ 2024/13-1.py
@@ -0,0 +1,14 @@
+import re
+
+total = 0
+
+for block in open('input.txt').read().split('\n\n'):
+    ax, ay, bx, by, px, py = map(int, re.findall(r'\d+', block))
+    ca = (px * by - py * bx) / (ax * by - ay * bx)
+    cb = (px * ay - py * ax) / (bx * ay - by * ax)
+
+    if ca % 1 == cb % 1 == 0:
+        if ca < 100 and cb < 100:
+            total += int(ca * 3 + cb)
+
+print(total)
