5473c134bb48a657170618f845496412f7f25586
Filip Malm-Bägén <Filip.bagen@gmail.com>
2025-01-05 10:39:21 -0800

    Day 19

---
+++ 2024/19-1.py
@@ -0,0 +1,24 @@
+lines = open('input.txt').read().splitlines()
+patterns = set(lines[0].split(', '))
+max_len = max(map(len, patterns))
+
+cache = {}
+
+
+def can_obtain(design):
+    if design == "":
+        return True
+
+    if design in cache:
+        return cache[design]
+
+    for i in range(min(len(design), max_len) + 1):
+        if design[:i] in patterns and can_obtain(design[i:]):
+            cache[design] = True
+            return True
+
+    cache[design] = False
+    return False
+
+
+print(sum(can_obtain(design) for design in lines[2:]))
