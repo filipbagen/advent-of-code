2c6289dfd32d9ac30b8fb000b0c12b0f7476f6b7
Filip Malm-Bägén <Filip.bagen@gmail.com>
2025-01-06 20:02:50 -0800

    Day 25

---
+++ 2024/25.py
@@ -0,0 +1,20 @@
+locks = []
+keys = []
+
+for block in open('input.txt').read().split('\n\n'):
+    grid = list(zip(*block.splitlines()))
+
+    if grid[0][0] == '#':
+        locks.append([row.count('#') - 1 for row in grid])
+    else:
+        keys.append([row.count('#') - 1 for row in grid])
+
+
+total = 0
+
+for lock in locks:
+    for key in keys:
+        if all(x + y <= 5 for x, y in zip(lock, key)):
+            total += 1
+
+print(total)
